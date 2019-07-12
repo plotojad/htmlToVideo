c2fa6ea70ac1e49d8890c87345606ee0f65a0fc3
plotojad <plotojad@gmail.com>
2019-07-12 22:25:24 +0300

    refactor class

--- src/main/java/chatToVid/ImageToVideo.java
+++ src/main/java/chatToVid/ImageToVideo.java
@@ -2,15 +2,36 @@ package chatToVid;
 
 import java.io.IOException;
 
-public class RecordAndEncodeVideo {
+public class ImageToVideo {
     Process process;
+    String mp3Path = null;
+    String videoPath = null;
+    String inputImagesFilePath;
+    InputFileForITV inputFileForITV;
+
+    public String getMp3Path() {
+        return mp3Path;
+    }
+
+    public String getVideoPath() {
+        return videoPath;
+    }
+
+    public ImageToVideo(String mp3Path, String videoPath) {
+        this.mp3Path = mp3Path;
+        this.videoPath = videoPath;
+    }
 
     public void imgToVideo() {
+
+        inputFileForITV = new InputFileForITV(XhtmlToImage.images);
+        inputFileForITV.makeFile();
+
         {
             try {
                 process = Runtime.getRuntime().exec("ffmpeg -f concat -safe 0 -i /home/plotoSUSE/input.txt -i " +
-                        "/home/plotoSUSE/IdeaProjects/htmlToVideo/POPDUBDeeBOP_Flame_on_wheels.mp3 -codec:a copy " +
-                        "-vcodec mpeg2video -pix_fmt yuv420p -vf fps=1 /home/plotoSUSE/Видео/OUTPUT.mp4");
+                        mp3Path + " -codec:a copy " +
+                        "-vcodec mpeg2video -pix_fmt yuv420p -vf fps=1 " + videoPath + "/OUTPUT.mp4");
             } catch (IOException e) {
                 e.printStackTrace();
             }
